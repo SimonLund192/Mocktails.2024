80618f27688762aac476f2ed189aa910e79e5af4
SimonLund192 <10503079@ucn.dk>
2024-11-19 21:34:45 +0100

    Start fra toppen, fjern UserRole i første omgang.

---
+++ Mocktails.WebApi/DTOs/UserDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace Mocktails.WebApi.DTOs;
+
+public class UserDTO
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string PasswordHash { get; set; }
+    public string Role { get; set; }
+}
