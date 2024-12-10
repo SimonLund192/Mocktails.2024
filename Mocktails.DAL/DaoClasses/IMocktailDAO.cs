8f6914c72aadb8e79981517208b425b7e69149e1
SimonLund192 <10503079@ucn.dk>
2024-12-10 19:21:43 +0100

    Cleaning up and doing some refactoring

--- Mocktails.DAL/DaoClasses/IMocktailDAO.cs
+++ Mocktails.DAL/DaoClasses/IMocktailDAO.cs
@@ -11,17 +11,20 @@ public interface IMocktailDAO
 {
     Task<IEnumerable<Mocktail>> GetMocktailsAsync();
     Task<IEnumerable<Mocktail>> GetTenLatestMocktailsAsync();
-
     /// <summary>
     /// Create a new Mocktail
     /// </summary>
     /// <param name="entity">The entity to create</param>
     /// <returns>The entity ID</returns>
     Task<int> CreateMocktailAsync(Mocktail entity);
+    /// <summary>
+    /// Updates the information on a specific mocktail
+    /// </summary>
+    /// <param name="entity"></param>
+    /// <returns>bool</returns>
     Task<bool> UpdateMocktailAsync(Mocktail entity);
     Task<bool> DeleteMocktailAsync(int id);
     Task<IEnumerable<Mocktail>> GetMocktailByPartOfNameOrDescription(string partOfNameOrDescription);
-
     Task<IEnumerable<Mocktail>> GetCategoryByIdAsync(int categoryId);
     Task<Mocktail> GetMocktailByIdAsync(int id);
     Task<bool> UpdateMocktailQuantityAsync(int id, int quantity);
--- Mocktails.Test/DAOTests/ShoppingCartDAOTests.cs
+++ Mocktails.Test/DAOTests/ShoppingCartDAOTests.cs
@@ -15,7 +15,7 @@ public class ShoppingCartDAOTests
     public void Setup()
     {
         // Use a test database connection string
-        var connectionString = "Server=MSI\\SQLEXPRESS;Database=MocktailsDB;Trusted_Connection=True;TrustServerCertificate=True;MultipleActiveResultSets=true;";
+        var connectionString = "Data Source=hildur.ucn.dk;Initial Catalog=DMA-CSD-S231_10462161;User ID=DMA-CSD-S231_10462161;Password=Password1!;TrustServerCertificate=True;";
         _cartDAO = new ShoppingCartDAO(connectionString);
     }
 }
